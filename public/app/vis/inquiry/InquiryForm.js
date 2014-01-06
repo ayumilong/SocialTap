447403e29c6f74095c7cd8a7c9339f951bfe92e5
Nick Watts <nick@nawatts.com>
2014-01-06 10:40:56 -0500

    Add advanced/basic toggle and search buttons.

--- public/app/vis/inquiry/InquiryForm.js
+++ public/app/vis/inquiry/InquiryForm.js
@@ -1,5 +1,6 @@
 define(['dojo/_base/declare',
 		'dojo/_base/lang',
+		'dojo/dom-class',
 		'dojo/dom-construct',
 		'dojox/mobile/Button',
 		'dojox/mobile/Pane',
@@ -8,12 +9,16 @@ define(['dojo/_base/declare',
 		'./GeoQueryFieldset',
 		'./SentimentQueryFieldset',
 		'./TextQueryFieldset'
-], function(declare, lang, domConstruct, Button, Pane, TextBox, DateQueryFieldset,
+], function(declare, lang, domClass, domConstruct, Button, Pane, TextBox, DateQueryFieldset,
 	GeoQueryFieldset, SentimentQueryFieldset, TextQueryFieldset)
 {
 	return declare([Pane], {
 
 		'class': 'inquiryForm',
+
+		advancedButton: null,
+
+		advancedNode: null,
 
 		buildRendering: function() {
 			this.inherited(arguments);
@@ -23,20 +28,49 @@ define(['dojo/_base/declare',
 			});
 			tqf.placeAt(this.domNode);
 
+			this.advancedNode = domConstruct.create('div', {
+				'class': 'advancedPane hidden'
+			}, this.domNode);
+
 			var dqf = new DateQueryFieldset({
 				label: 'Filter by post date'
 			});
-			dqf.placeAt(this.domNode);
+			dqf.placeAt(this.advancedNode);
 
 			var sqf = new SentimentQueryFieldset({
 				label: 'Filter by sentiment'
 			});
-			sqf.placeAt(this.domNode);
+			sqf.placeAt(this.advancedNode);
 
 			var gqf = new GeoQueryFieldset({
 				label: 'Filter by location'
 			});
-			gqf.placeAt(this.domNode);
+			gqf.placeAt(this.advancedNode);
+
+			var searchButton = new Button({
+				'class': 'searchButton button',
+				duration: 0,
+				label: 'Search'
+			});
+			searchButton.placeAt(this.domNode);
+
+			this.advancedButton = new Button({
+				'class': 'advancedButton button',
+				label: 'Advanced',
+				duration: 0,
+				onClick: lang.hitch(this, function() {
+					domClass.toggle(this.advancedNode, 'hidden');
+					if (domClass.contains(this.advancedNode, 'hidden')) {
+						this.advancedButton.set('label', 'Advanced');
+					}
+					else {
+						this.advancedButton.set('label', 'Basic');
+					}
+				})
+			});
+			this.advancedButton.placeAt(this.domNode);
+
+
 		}
 
 	});
