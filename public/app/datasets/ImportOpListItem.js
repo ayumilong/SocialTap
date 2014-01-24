c921fdee03af871c6c208c2729426d3994c93034
Nick Watts <nick@nawatts.com>
2014-01-24 11:49:22 -0500

    Add time zone to import start/stop times.

--- public/app/datasets/ImportOpListItem.js
+++ public/app/datasets/ImportOpListItem.js
@@ -1,6 +1,7 @@
 define(['dojo/_base/declare',
+		'dojo/date/locale',
 		'dojo-mama/util/BaseListItem'],
-function(declare, BaseListItem) {
+function(declare, locale, BaseListItem) {
 	return declare([BaseListItem], {
 
 		'class': 'importOpListItem',
@@ -13,15 +14,20 @@ function(declare, BaseListItem) {
 			var startTime = new Date(Date.parse(importOp.time_started));
 			var stopTime = (importOp.time_stopped !== null) ? new Date(Date.parse(importOp.time_stopped)) : null;
 
+			var fmt = {
+				datePattern: 'EEE M/d/yyy',
+				timePattern: 'h:mm:ss a z'
+			};
+
 			var text = '';
-			text += '<span class="startTime">Started ' + startTime.toLocaleDateString() + ' ' + startTime.toLocaleTimeString() + '</span>';
+			text += '<span class="startTime">Started ' + locale.format(startTime, fmt) + '</span>';
 			text += '<br>';
 			if (importOp.in_progress) {
 				text += '<span class="status"><span style="color: green;">In Progress</span></span>';
 			}
 			else {
 				var stopMessage = (importOp.failed) ? 'Stopped ' : 'Completed ';
-				stopMessage += stopTime.toLocaleDateString() + ' ' + stopTime.toLocaleTimeString();
+				stopMessage += locale.format(stopTime, fmt);
 				if (importOp.failed) {
 					stopMessage = '<span style="color: red;">' + stopMessage + ' (' + importOp.error_message + ')</span>';
 				}
