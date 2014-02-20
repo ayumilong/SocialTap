6ce90a628ce80cbbaaf316dc46c006dd65a36af3
Nick Watts <nick@nawatts.com>
2014-02-20 10:09:35 -0500

    Add button to inquiry form to generate a report for that inquiry.

--- app/controllers/api/v0/reports_controller.rb
+++ app/controllers/api/v0/reports_controller.rb
@@ -7,7 +7,7 @@ class Api::V0::ReportsController < ApplicationController
 	def index
 		@reports = current_user.reports
 
-		render json: @reports.as_json(:methods => [:output_path])
+		render json: @reports.as_json(:include => [:dataset], :methods => [:output_path])
 	end
 
 	# GET /api/v0/reports/1
@@ -15,17 +15,29 @@ class Api::V0::ReportsController < ApplicationController
 	def show
 		@report = current_user.reports.find_by_id(params[:id])
 
-		render json: @report.as_json(:methods => [:output_path])
+		render json: @report.as_json(:include => [:dataset], :methods => [:output_path])
 	end
 
 	# POST /api/v0/reports
 	# POST /api/v0/reports.json
 	def create
+
+		# By default, Rails converts empty arrays in the JSON request to nil.
+		# This uses ActiveSupport::JSON#decode to undo that.
+		if request.format.json?
+			request.body.rewind
+			request_body = request.body.read
+			unless request_body.blank?
+				request_body = ActiveSupport::JSON.decode(request_body)
+				params[:report][:inquiry_definition].merge!(request_body['report']['inquiry_definition'])
+			end
+		end
+
 		@report = Report.new(params[:report])
 		@report.user = current_user
 
 		if @report.save
-			render json: @report.as_json(:methods => [:output_path]), status: :created, location: @report
+			render json: @report.as_json(:include => [:dataset], :methods => [:output_path]), status: :created, location: [:api, :v0, @report]
 		else
 			render json: @report.errors, status: :unprocessable_entity
 		end
