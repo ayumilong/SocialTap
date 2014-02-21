d2d4ca8652fc8d068661f960791cb9da7154678a
Chris Guirl <guirl@clemson.edu>
2014-02-21 15:12:21 -0500

    Updates to Historical PowerTrack library and more work on analysis framework

--- lib/analysis.rb
+++ lib/analysis.rb
@@ -7,7 +7,9 @@
 
 require 'json'
 require 'elasticsearch'
-require 'amqp'
+require 'bunny'
+require './lib/analyzers/sentiment140_analyzer.rb'
+
 
 module SocialTap
   class Analysis
@@ -18,65 +20,84 @@ module SocialTap
         host: APP_CONFIG["Elasticsearch"]["hostname"],
         port: APP_CONFIG["Elasticsearch"]["port"],
         log: false)
-      @running = true
+      # create queues
+      self.setup_queues
       # load all the analyzers present, creating pool of worker processes
       self.create_analyzer_pool
       # start main loop
   	  self.start
   	end
 
-    # find all the documents in Elasticsearch which need analysis
-  	def select_posts limit=nil
-      # get list of indices to search 
-      indices = []
-      Dataset.find(:all).each do |dataset|
-        indices << dataset.es_index
-      end
-  	  # build query for new documents. documents with no SocialTap object
-      # haven't been analyzed yet
-  	  missing_docs_query = {
-    		"filtered" => {
-    		  "query" => {
-    		  	"match_all" => {}
-    		  },
-    		  "filter" => {
-    		    "missing" => {
-    		      "field" => "SocialTap",
-    		      "existence" => true
-    		    }
-    		  }
-    		}
-      }
-      # get results from ES
-      missing_docs = @es_client.search query: missing_docs_query
-  	  # TODO: get posts that have been flagged for reprocessing
-      #docs_to_reprocess = {}
-      missing_docs
-  	end
+    # connect to RabbitMQ, and create I/O queues for workers
+    def setup_queues
+      @rabbitmq = Bunny.new
+      @rabbitmq.start
+      @channel = @rabbitmq.create_channel
+      @input_exchange = @channel.fanout("analysis.new_documents")
+      @results_exchange = @channel.topic("analysis.results")
+    end
+
+    # load all the analyzers present and create a pool of worker processes
+    def create_analyzer_pool
+      # get glob of filenames in post processing dir
+      # for each file,
+        # ruby-require file
+        # read Analyzer class names from module
+        # start configured number of workers for each Analyzer
+      @analyzers = []
+      @analyzers << SocialTap::Analyzers::Sentiment140.new
+    end
 
     # instatiate a single analyzer process
     def create_analyzer_worker
-      # create interprocess messaging queue 
+      # subscribe to results messaging queue 
+      output_queue = @channel.queue("analysis.results").bind(@results_exchange, routing_key: "analysis.results.#.#")
+      output_queue.subscribe do |delivery_info, properties, payload|
+        self.store_output delivery_info, properties, payload
+      end
       # start new process
         # instantiate new object
       # store pid of new worker's process, analyzer name, messaging queue
     end
 
-    # load all the analyzers present and create a pool of worker processes
-  	def create_analyzer_pool
-  	  # get glob of filenames in post processing dir
-  	  # for each file,
-  	    # ruby-require file
-  	    # read Analyzer class names from module
-  	    # start configured number of workers for each Analyzer
-
-  	end
+    # find all the documents in Elasticsearch which need analysis
+    def select_posts limit=nil
+      # get list of indices to search 
+      indices = []
+      Dataset.find(:all).each do |dataset|
+        indices << dataset.es_index
+      end
+      # build query for new documents. documents with no SocialTap object
+      # haven't been analyzed yet
+      missing_docs_query = {
+        "filtered" => {
+          "query" => {
+            "match_all" => {}
+          },
+          "filter" => {
+            "missing" => {
+              "field" => "SocialTap",
+              "existence" => true
+            }
+          }
+        }
+      }
+      # get results from ES
+      missing_docs = @es_client.search query: missing_docs_query
+      # TODO: get posts that have been flagged for reprocessing
+      #docs_to_reprocess = {}
+      missing_docs
+    end
 
     # main loop to keep checking for things to process
   	def start
+      @running = true
       while @running
         # check for posts to process
-        posts_to_process = select_posts
+        posts_to_process = self.select_posts
+        posts_to_process.each do |post|
+          @input_exchange.publish post
+        end
         # for each analyzer type,
           # while there are more posts to send this analyzer
             # for each worker,
@@ -93,25 +114,41 @@ module SocialTap
       @running = false
   	end 
     
+    def store_output delivery_info, properties, payload
+      # lookup document by ID in ES10
+      document = JSON[payload]
+      puts "received a store output message:"
+      pp document
+    end
   end
 
   # parent class for Analyzers
   class Analyzer
 
     # setup new Analyzer
-  	def initialize name, worker = 0
+  	def initialize analyzer_name, id
       # subclasses may implement this, but should call super
-      @name = name
-      @worker = worker
-      self.setup_results_queue
+      @name = analyzer_name
+      @id = id
+      self.setup_queues
       self.start
   	end
 
     # creates the communication queue for sending results to parent process
-    def setup_results_queue
-      # create new queue
+    def setup_queues
+      @rabbitmq = Bunny.new
+      @rabbitmq.start
+      @channel = @rabbitmq.create_channel
+      @input_exchange = @channel.fanout("analysis.new_documents")
+      @results_exchange = @channel.topic("analysis.results")
+      # connect to input queue
+      @input_queue = @channel.queue("input#{i}").bind(input_exchange)
+      @input_queue.subscribe do |delivery_info, properties, payload|
+        self.new_document delivery_info, properties, payload
+      end
+    end
 
-      # tell parent process about the queue?
+    def new_document delivery_info, properties, payload
     end
 
     # initiate main data analysis process loop
@@ -125,17 +162,30 @@ module SocialTap
       @running = false
     end
 
-    # main data analysis loop: handle new data coming into the queue and
-    # prepare for analysis
+    # loop until further notice
     def run
-      # subclasses should implement this
-      # simplest case: dequeue new documents to analyze, call analyze on all
+      while @running
+        self.step
+      end
+    end
+
+    def step
     end
 
     # perform analysis on documents handled by main loop
+    # subclasses should implement this
   	def analyze documents
-  	  # subclasses should implement this
   	end
+
+    def store_output documents
+      documents.each do |document|
+        @results_exchange.publish document, routing_key: "analysis.results.#{@name}.#{@id}"
+      end
+    end
   end
 
 end
+
+
+SocialTap::Analysis.new
+
--- lib/analyzers/sentiment140_analyzer.rb
+++ lib/analyzers/sentiment140_analyzer.rb
@@ -5,41 +5,44 @@ require './lib/sentiment140'
 
 ### SocialTap Analysis Framework Options ###
 # The class(es) defined by this module
-ANALYZER_CLASSES = ["OpenClemson::Sentiment140"]
 
 # analyzer for sentiment140.com sentiment tagging
+
 BULK_SENTIMENT_QTY = 2000
 
-module OpenClemson
-  class Sentiment140 < SocialTap::Analyzer
+module SocialTap
+  module Analyzers
+    class Sentiment140 < SocialTap::Analyzer
 
-    def initialize
-      @s140_client = Sentiment140::QueryAPI.new APP_CONFIG['Sentiment140']['app_id']
-      @document_cache = []
-      super # thanks for asking
-    end
+      def initialize
+        @s140_client = Sentiment140::QueryAPI.new APP_CONFIG['Sentiment140']['app_id']
+        @document_cache = []
+        super # thanks for asking
+      end
 
-    def run
-      # main loop for this process
-      while @running
-        # check communication queue for new documents
-          # put new documents in the wait list
-        # check queue size - if BULK_SENTIMENT_QTY is reached 
-          # analyze all waiting documents
+      def step
+        sleep 0.5
+        puts "this might be useful eventually"
+      end
+
+      def new_document delivery_info, properties, payload
+        document = JSON[payload]
+        document["SocialTap"] ||= {}
+        document["SocialTap"]["Sentiment140"] = "analyzed"
+        self.store_output document
+      end
+
+      def analyze documents
+        # collect query data for Sentiment140
+        # for each document,
+          # build Sentiment140 query data
+        # send query off to Sentiment140
+        # convert results from JSON
+        # for each result, 
+          # build results for this analyzer
+          # store by Elasticsearch document id
+        # store document analysis data by calling store_output
       end
     end
-
-    def analyze documents
-      # collect query data for Sentiment140
-      # for each document,
-        # build Sentiment140 query data
-      # send query off to Sentiment140
-      # convert results from JSON
-      # for each result, 
-        # build results for this analyzer
-        # store by Elasticsearch document id
-      # store document analysis data by calling store_output
-    end
-
   end
 end
