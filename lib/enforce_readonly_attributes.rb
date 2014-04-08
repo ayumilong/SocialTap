936c8e0357521b19398050c69376b71a2cc4cd5f
Nick Watts <nick@nawatts.com>
2014-04-08 09:32:25 -0400

    Return frozen objects for readonly attributes.

--- lib/enforce_readonly_attributes.rb
+++ lib/enforce_readonly_attributes.rb
@@ -15,16 +15,24 @@ module EnforceReadonlyAttributes
 
 				# For each attribute declared readonly, override its setter to raise an exception
 				# if the record has already been persisted.
-				attributes.each do |attr|
-					self.class_eval %Q"
-						def #{attr}=(value)
+				attributes.each do |attr_name|
+					self.class_eval <<-eoruby
+						def #{attr_name}=(value)
 							if !persisted?
 								super
 							else
-								raise ReadonlyAttributeError, 'Attempt to modify readonly attribute #{attr} on #{self.name}'
+								raise ReadonlyAttributeError, "Attempt to modify readonly attribute #{attr_name} on #{self.name}"
 							end
 						end
-					"
+
+						def #{attr_name}
+							if !persisted?
+								super
+							else
+								super.freeze
+							end
+						end
+					eoruby
 				end
 			end
 		end
