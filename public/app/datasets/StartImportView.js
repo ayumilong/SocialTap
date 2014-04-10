5376048accb571e82e88de4ce127acb6f5b64c44
Nick Watts <nick@nawatts.com>
2014-04-10 10:36:25 -0400

    Load available format conversions from API. Only show conversions that are relevant to the source type.

--- public/app/datasets/StartImportView.js
+++ public/app/datasets/StartImportView.js
@@ -5,11 +5,12 @@ define(['dojo/_base/declare',
 		'dojo/dom-construct',
 		'dojo/request/xhr',
 		'dojo/text!./StartImportView.html',
+		'dojo/when',
 		'dojo-mama/views/_ModuleViewMixin',
 		'dijit/_WidgetBase',
 		'dijit/_TemplatedMixin',
 		'app/util/Form'
-], function(declare, lang, domAttr, domClass, domConstruct, xhr, template, _ModuleViewMixin,
+], function(declare, lang, domAttr, domClass, domConstruct, xhr, template, when, _ModuleViewMixin,
 	_WidgetBase, _TemplatedMixin, Form)
 {
 	return declare([_WidgetBase, _TemplatedMixin, _ModuleViewMixin], {
@@ -40,43 +41,70 @@ define(['dojo/_base/declare',
 				this.form.destroy();
 			}
 
+			var typeFields = [];
 			if (sourceType === 'file') {
+				typeFields = [
+					{
+						label: 'File to be imported',
+						name: 'source_spec.path',
+						placeholder: '/path/to/file/',
+						type: 'text'
+					},
+				];
+			}
+			else if (sourceType === 'gnip') {
+				typeFields = [
+					{
+						label: 'Rule',
+						name: 'source_spec.rule',
+						placeholder: 'Gnip rule',
+						type: 'text'
+					}
+				];
+			}
+
+			when(this.loadConversions(), lang.hitch(this, function(conversions) {
+
+				// Filter conversions based on source type
+				var typeConversions = [];
+				conversions.forEach(function(conversion) {
+					if ((sourceType === 'file') ||
+						(sourceType === 'gnip' && conversion.from === 'activity_stream') ||
+						(sourceType === 'twitter' && conversion.from === 'twitter'))
+					{
+						typeConversions.push(conversion);
+					}
+				});
+
 				this.form = new Form({
 					fields: [
 						{
 							type: 'hidden',
 							name: 'source_type',
-							value: 'file'
-						},
-						{
-							label: 'File to be imported',
-							name: 'source_spec.path',
-							placeholder: '/path/to/file/',
-							type: 'text'
-						},
+							value: sourceType
+						}
+					].concat(typeFields)
+					.concat([
 						{
 							label: 'Convert imported data',
 							name: 'source_spec.convert',
 							type: 'checkbox'
 						},
-						// TODO: From/To format options should be loaded from /api/v0/imports/format_conversions
 						{
 							label: 'From',
 							name: 'source_spec.from_format',
 							type: 'select',
-							options: [
-								{ label: 'Twitter', value: 'twitter' }
-							]
+							options: typeConversions.map(function(c) { return c.from; }).uniqueValues().map(function(format) {
+								return { label: format.formatSymbolForDisplay(), value: format };
+							})
 						},
 						{
 							label: 'To',
 							name: 'source_spec.to_format',
 							type: 'select',
-							options: [
-								{ label: 'Activity Stream', value: 'activity_stream' }
-							]
+							options: []
 						}
-					],
+					]),
 					modifyValue: function(val) {
 						if (val.source_spec.convert === false) {
 							delete val.source_spec.from_format;
@@ -88,9 +116,14 @@ define(['dojo/_base/declare',
 					url: '/api/v0/datasets/' + this.datasetId + '/imports'
 				});
 
+				// Hide 'Convert' option if there are no conversions available
+				domClass[(typeConversions.length === 0) ? 'add' : 'remove'](this.form.fields[2].containerNode, 'hidden');
+
 				domClass.add(this.form.fields[2].containerNode, 'inline');
 				domClass.add(this.form.fields[3].containerNode, 'inline hidden');
 				domClass.add(this.form.fields[4].containerNode, 'inline hidden');
+
+				this.updateConversionToOptions(this.form.fields[3].domNode.value);
 
 				this.form.on('field_change', lang.hitch(this, function(e) {
 					if (e.field === 'source_spec.convert') {
@@ -98,38 +131,47 @@ define(['dojo/_base/declare',
 						f(this.form.fields[3].containerNode, 'hidden');
 						f(this.form.fields[4].containerNode, 'hidden');
 					}
+
+					if (e.field === 'source_spec.from_format') {
+						this.updateConversionToOptions(e.value);
+					}
 				}));
-			}
 
-			else if (sourceType === 'gnip') {
-				this.form = new Form({
-					fields: [
-						{
-							type: 'hidden',
-							name: 'source_type',
-							value: 'gnip'
-						},
-						{
-							label: 'Rule',
-							name: 'source_spec.rule',
-							placeholder: 'Gnip rule',
-							type: 'text'
-						}
-					],
-					name: 'import_operation',
-					url: '/api/v0/datasets/' + this.datasetId + '/imports'
-				});
-			}
+				this.form.on('success', lang.hitch(this, function() {
+					this.router.go('/' + this.datasetId);
+				}));
 
-			this.form.on('success', lang.hitch(this, function() {
-				this.router.go('/' + this.datasetId);
+				this.form.placeAt(this.formContainerNode);
 			}));
-
-			this.form.placeAt(this.formContainerNode);
 		},
 
 		cancelButtonClicked: function() {
 			this.router.go('/' + this.datasetId);
+		},
+
+		loadConversions: function() {
+			if (this.conversions !== undefined) {
+				return this.conversions;
+			}
+			else {
+				return xhr.get('/api/v0/imports/format_conversions', {
+					handleAs: 'json'
+				}).then(lang.hitch(this, function(conversions) {
+					this.conversions = conversions;
+					return conversions;
+				}));
+			}
+		},
+
+		updateConversionToOptions: function(fromFormat) {
+			var toFormats = this.conversions.filter(function(c) { return c.from === fromFormat; }).map(function(c) { return c.to; }).uniqueValues();
+			domConstruct.empty(this.form.fields[4].domNode);
+			toFormats.forEach(lang.hitch(this, function(f) {
+				domConstruct.create('option', {
+					innerHTML: f.formatSymbolForDisplay(),
+					value: f
+				}, this.form.fields[4].domNode);
+			}));
 		}
 
 	});
--- public/app/util/extensions.js
+++ public/app/util/extensions.js
@@ -6,4 +6,16 @@ define([], function() {
 		return this.charAt(0).toUpperCase() + this.slice(1);
 	};
 
+	String.prototype.formatSymbolForDisplay = function() {
+		// summary:
+		//     Converts strings like 'activity_stream' to 'Activity Stream'
+		return this.replace(/_([a-z])/g, function(match, letter) { return " " + letter.toUpperCase(); }).capitalize();
+	};
+
+	Array.prototype.uniqueValues = function() {
+		return this.filter(function(value, index, self) {
+			return self.indexOf(value) === index;
+		});
+	};
+
 });
